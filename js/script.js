970a1aa306e4cbd249f43fa08706c0310654dcb4
KimJY <kjy3501@naver.com>
2022-06-24 09:43:50 +0900

    https://github.com/devjy1550/Masigray.git

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 // html,css,js 로딩완료
-$(document).ready();
+$(document).ready(function () {
+  // 메인메뉴기능
+  let header = $('.header');
+  let header_top = $('.header-top');
+  let header_main = $('.header-main');
+  let gnb = $('.gnb');
+  gnb.mouseenter(function () {
+    header_main.addClass('header-main-show');
+  });
+  gnb.mouseleave(function(){
+    header_main.removeClass('header-main-show'); 
+  });
+  //스크롤시 기능
+  $(window).scroll(function () {
+    //스크롤바의 위치를 파악한다.
+    let temp = $(window).scrollTop();
+    // .header-top 숨기기
+    if (temp > 0) {
+      header.addClass('header-fix');
+      header_top.addClass('header-top-close')
+    } else {
+      header.removeClass('header-fix');
+      header_top.removeClass('header-top-close')
+    }
+
+
+
+
+  });
+});
 
 // html,css,js 멀티미디어까지 로딩완료
 window.onload = function () {
@@ -38,22 +67,21 @@ window.onload = function () {
   });
 
 
-new Swiper('.sw-beverage', {
-  loop: true,
-  autoplay: {
-    delay: 1800,
-    disableOnInteraction: false
-  },
-  navigation: {
-    prevEl: '.sw-beverage-prev',
-    nextEl: '.sw-beverage-next'
-  },
-  pagination: {
-    el: '.sw-beverage-pgnt',
-    type: 'fraction',
-    //이걸 꼭 쳐줘야지 숫자로 페이지네이션이 바뀐다!!!
-    // clickable: true,
-  },
-});
-
+  new Swiper('.sw-beverage', {
+    loop: true,
+    autoplay: {
+      delay: 1800,
+      disableOnInteraction: false
+    },
+    navigation: {
+      prevEl: '.sw-beverage-prev',
+      nextEl: '.sw-beverage-next'
+    },
+    pagination: {
+      el: '.sw-beverage-pgnt',
+      type: 'fraction',
+      //이걸 꼭 쳐줘야지 숫자로 페이지네이션이 바뀐다!!!
+      // clickable: true,
+    },
+  });
 }
